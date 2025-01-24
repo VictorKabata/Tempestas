a71ed043c67bf287532e12e80a7e877d4d50a69e
Victor Kabata <victorbro14@gmail.com>
2025-01-24 16:14:34 +0300

    Set up detekt and ktLint

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,13 +1,15 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("java-library")
-    alias(libs.plugins.jetbrains.kotlin.jvm)
+    // alias(libs.plugins.kotlin.jvm)
 }
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
 }
-kotlin {
+/*kotlin {
     compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+        jvmTarget = JvmTarget.JVM_11
     }
-}
+}*/
