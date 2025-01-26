6fd5a9f203f9548f0d8a7be4d856c6aaae857c70
Victor Kabata <victorbro14@gmail.com>
2025-01-26 07:54:31 +0300

    Set up navigation

--- shared/src/commonMain/kotlin/com/vickbt/shared/ui/navigation/Navigation.kt
+++ shared/src/commonMain/kotlin/com/vickbt/shared/ui/navigation/Navigation.kt
@@ -5,13 +5,16 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.vickbt.shared.ui.screens.home.HomeScreen
 
 @Composable
-fun Navigation(navController: NavHostController, paddingValues: PaddingValues) {
+fun Navigation(paddingValues: PaddingValues) {
+    val navController: NavHostController = rememberNavController()
+
     NavHost(navController = navController, startDestination = NavigationItem.Home.route) {
         composable(route = NavigationItem.Home.route) {
-            HomeScreen()
+            HomeScreen(paddingValues=paddingValues)
         }
     }
 }
--- shared/src/commonMain/kotlin/com/vickbt/shared/ui/screens/home/HomeScreen.kt
+++ shared/src/commonMain/kotlin/com/vickbt/shared/ui/screens/home/HomeScreen.kt
@@ -1,7 +1,14 @@
 package com.vickbt.shared.ui.screens.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(paddingValues:PaddingValues) {
+    Box(modifier = Modifier.fillMaxSize().background(color = Color.Red))
 }
