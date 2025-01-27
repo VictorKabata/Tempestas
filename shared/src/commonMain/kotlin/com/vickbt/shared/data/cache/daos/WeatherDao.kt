9743fd5393c75b243f59944928f3f28aebc9cfe9
Victor Kabata <victorbro14@gmail.com>
2025-01-27 05:15:57 +0300

    Create WeatherDao.kt

---
+++ shared/src/commonMain/kotlin/com/vickbt/shared/data/cache/daos/WeatherDao.kt
@@ -0,0 +1,18 @@
+package com.vickbt.shared.data.cache.daos
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.vickbt.shared.data.cache.entities.WeatherDataEntity
+
+@Dao
+interface WeatherDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertWeatherData(weatherItems: List<WeatherDataEntity>)
+
+    @Query("SELECT * FROM weather_data_table")
+    suspend fun getWeatherData(): List<WeatherDataEntity>
+
+}
