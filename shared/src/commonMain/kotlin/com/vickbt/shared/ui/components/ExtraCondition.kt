326cfdecab220837363035c28196f59d3b93f52a
Victor Kabata <victorbro14@gmail.com>
2025-01-27 20:45:42 +0300

    Linting

--- shared/src/commonMain/kotlin/com/vickbt/shared/ui/components/ExtraCondition.kt
+++ shared/src/commonMain/kotlin/com/vickbt/shared/ui/components/ExtraCondition.kt
@@ -1,7 +1,6 @@
 package com.vickbt.shared.ui.components
 
 import android.graphics.drawable.Icon
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -15,15 +14,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil3.compose.AsyncImage
 
 @Composable
 fun ExtraCondition(
